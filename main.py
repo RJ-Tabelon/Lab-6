9f2be9001055d6754265fa42da9f03e89b0fb770
Dylanskyep <dpanganiban@ufl.edu>
2023-10-31 12:03:01 -0400

    decode function

--- main.py
+++ main.py
@@ -16,20 +16,28 @@ def encode(password):
             new_pass += i
     return new_pass
 
+def decode(password):
+    decoded = ""
+    for char in password:
+        decode_char = str((int(char) - 3) % 10)
+        decoded += decode_char
+    return decoded
+
 def main():
-    print("Menu")
-    print("-------------")
-    print("1. Encode")
-    print("2. Decode")
-    print("3. Quit")
     while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
         option = int(input("Please enter an option: "))
         if option == 1:
             password = input("Please enter your password to encode: ")
             new_pass = encode(password)
             print("Your password has been encoded and stored!")
         if option == 2:
-            print(f"The encoded password is {new_pass}, and the original password is {variable}")
+            org_pass = decode(new_pass)
+            print(f"The encoded password is {new_pass}, and the original password is {org_pass}.")
         if option == 3:
             break
 
